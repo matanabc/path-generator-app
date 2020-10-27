bdcb4a2b6032b065a0a555864dc377eb7a3d7a04
Matan Steinmetz <matanabc@gmail.com>
2020-10-27 09:14:03 +0200

    Add: canvas-painter

---
+++ src/component/field-view/canvas-painter.js
@@ -0,0 +1,103 @@
+export const drawOnCanvas = (canvas, props) => {
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (props.fieldConfig === undefined) return;
+    if (props.paths.length === 0) return;
+    if (!props.path) return;
+
+    drawFieldBorders(ctx, props);
+    drawWaypoints(ctx, props);
+    drawSetpoints(ctx, props);
+    drawRobot(ctx, props);
+    drawPathTotalTime(ctx, props);
+}
+
+function drawFieldBorders(ctx, props) {
+    ctx.beginPath();
+    ctx.rect(props.fieldConfig.topLeftXPixel, props.fieldConfig.topLeftYPixel,
+        props.fieldConfig.widthInPixel, props.fieldConfig.heigthInPixel);
+    ctx.strokeStyle = "blue";
+    ctx.stroke();
+}
+
+function drawWaypoints(ctx, props) {
+    props.paths[props.pathID].waypoints.forEach(waypoint => {
+        const x = waypoint.x / props.fieldConfig.widthPixelToMeter + props.fieldConfig.topLeftXPixel;
+        const y = waypoint.y / props.fieldConfig.hightPixelToMeter + props.fieldConfig.topLeftYPixel;
+        ctx.beginPath();
+        ctx.arc(x, y, 2, 0, Math.PI * 2, false);
+        ctx.fillStyle = "red";
+        ctx.fill();
+    });
+}
+
+function drawSetpoints(ctx, props) {
+    ctx.beginPath();
+    props.path.leftSetpoints.forEach((setpoint, index) => {
+        const x = setpoint.x / props.fieldConfig.widthPixelToMeter + props.fieldConfig.topLeftXPixel;
+        const y = setpoint.y / props.fieldConfig.hightPixelToMeter + props.fieldConfig.topLeftYPixel;
+        if (index === 0)
+            ctx.moveTo(x, y);
+        else
+            ctx.lineTo(x, y);
+    });
+    props.path.rightSetpoints.forEach((setpoint, index) => {
+        const x = setpoint.x / props.fieldConfig.widthPixelToMeter + props.fieldConfig.topLeftXPixel;
+        const y = setpoint.y / props.fieldConfig.hightPixelToMeter + props.fieldConfig.topLeftYPixel;
+        if (index === 0)
+            ctx.moveTo(x, y);
+        else
+            ctx.lineTo(x, y);
+    });
+    ctx.strokeStyle = "Yellow";
+    ctx.stroke();
+}
+
+function drawPathTotalTime(ctx, props) {
+    const pathSetpointLength = props.path.sourceSetpoints.length;
+    const robotLoopTime = props.pathConfig.robotLoopTime;
+    const pathTotalTime = (pathSetpointLength * robotLoopTime).toFixed(2);
+    ctx.font = "10px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillRect(4, 3, 21, 10);
+    ctx.fillStyle = "white";
+    ctx.fillText(`${pathTotalTime}`, 5, 12);
+}
+
+function drawRobot(ctx, props) {
+    const setpoint = props.path.sourceSetpoints[props.rangePosition];
+    const robotInReverse = props.paths[props.pathID].isInReverse ? -1 : 1;
+    const robotLength = Number(props.robotDrawConfig.length) / props.fieldConfig.widthPixelToMeter;
+    const robotWidth = Number(props.robotDrawConfig.width) / props.fieldConfig.hightPixelToMeter;
+    const robotCenter = robotInReverse * Number(props.robotDrawConfig.center) / props.fieldConfig.widthPixelToMeter;
+    const x = (setpoint.x / props.fieldConfig.widthPixelToMeter) - (robotLength / 2) +
+        props.fieldConfig.topLeftXPixel;
+    const y = (setpoint.y / props.fieldConfig.hightPixelToMeter) - (robotWidth / 2) +
+        props.fieldConfig.topLeftYPixel;
+
+    ctx.beginPath();
+    ctx.save();
+    ctx.translate(x + (robotLength / 2), y + (robotWidth / 2));
+    ctx.rotate(setpoint.heading);
+    ctx.fillStyle = "white";
+    ctx.fillRect((-robotLength / 2) - robotCenter, (-robotWidth / 2), robotLength, robotWidth);
+
+    ctx.arc(robotCenter, 0, 1.5, 0, Math.PI * 2, false);
+    ctx.fillStyle = "rgb(50, 100, 0)";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo((robotInReverse * (robotLength / 2) - robotCenter), (-robotWidth / 2));
+    ctx.lineTo((robotInReverse * (robotLength / 2) - robotCenter), (robotWidth / 2));
+    ctx.strokeStyle = "blue";
+    ctx.stroke(); 
+
+    ctx.beginPath();
+    ctx.moveTo((robotInReverse * (-robotLength / 2) - robotCenter), (-robotWidth / 2));
+    ctx.lineTo((robotInReverse * (-robotLength / 2) - robotCenter), (robotWidth / 2));
+    ctx.strokeStyle = "red";
+    ctx.stroke(); 
+
+    ctx.restore();
+}
--- src/component/field-view/field-view.js
+++ src/component/field-view/field-view.js
@@ -3,103 +3,27 @@ import { connect } from 'react-redux';
 import { addWaypoint, setPath } from "./field-view-action";
 import { PathWaypoint } from "../../path-generator/path-generator";
 import PlayingBar from "../playing-bar/playing-bar";
+import { drawOnCanvas } from "./canvas-painter";
 
 class FieldView extends React.Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
-    this.drawFieldBorders = this.drawFieldBorders.bind(this);
-    this.drawWaypoints = this.drawWaypoints.bind(this);
-    this.drawWaypoints = this.drawWaypoints.bind(this);
     this.whenClick = this.whenClick.bind(this);
-    this.drawField = this.drawField.bind(this);
-    this.drawRobot = this.drawRobot.bind(this);
   }
 
   componentDidMount() {
-    this.drawField();
+    drawOnCanvas(this.canvasRef.current, this.props);
   }
 
-  drawField() {
-    if (this.props.fieldConfig === undefined) return;
-    if (this.props.paths.length === 0) return;
+  componentDidUpdate() {
     const canvas = this.canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.drawFieldBorders(canvas);
-    this.drawWaypoints(ctx);
-  }
+    if (this.props.listenToMouseClicks)
+      canvas.addEventListener('mousedown', this.whenClick);
+    else
+      canvas.removeEventListener('mousedown', this.whenClick);
 
-  drawFieldBorders(canvas) {
-    const ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.rect(this.props.fieldConfig.topLeftXPixel, this.props.fieldConfig.topLeftYPixel,
-      this.props.fieldConfig.widthInPixel, this.props.fieldConfig.heigthInPixel);
-    ctx.strokeStyle = "blue";
-    ctx.stroke();
-  }
-
-  drawWaypoints(ctx) {
-    this.props.paths[this.props.pathID].waypoints.forEach(waypoint => {
-      const x = waypoint.x / this.props.fieldConfig.widthPixelToMeter + this.props.fieldConfig.topLeftXPixel;
-      const y = waypoint.y / this.props.fieldConfig.hightPixelToMeter + this.props.fieldConfig.topLeftYPixel;
-      ctx.beginPath();
-      ctx.arc(x, y, 2, 0, Math.PI * 2, false);
-      ctx.fillStyle = "red";
-      ctx.fill();
-    });
-    this.drawPath(ctx)
-  }
-
-  drawPath(ctx) {
-    if (!this.props.path) return;
-    const config = this.props.pathConfig;
-    const path = this.props.path;
-    ctx.beginPath();
-    path.leftSetpoints.forEach((setpoint, index) => {
-      const x = setpoint.x / this.props.fieldConfig.widthPixelToMeter + this.props.fieldConfig.topLeftXPixel;
-      const y = setpoint.y / this.props.fieldConfig.hightPixelToMeter + this.props.fieldConfig.topLeftYPixel;
-      if (index === 0)
-        ctx.moveTo(x, y);
-      else
-        ctx.lineTo(x, y);
-    });
-    path.rightSetpoints.forEach((setpoint, index) => {
-      const x = setpoint.x / this.props.fieldConfig.widthPixelToMeter + this.props.fieldConfig.topLeftXPixel;
-      const y = setpoint.y / this.props.fieldConfig.hightPixelToMeter + this.props.fieldConfig.topLeftYPixel;
-      if (index === 0)
-        ctx.moveTo(x, y);
-      else
-        ctx.lineTo(x, y);
-    });
-
-    ctx.strokeStyle = "Yellow";
-    ctx.stroke();
-    ctx.font = "10px Arial";
-    ctx.fillStyle = "black";
-    ctx.fillRect(4, 3, 21, 10);
-    ctx.fillStyle = "white";
-    ctx.fillText(`${(path.sourceSetpoints.length * config.robotLoopTime).toFixed(2)}`, 5, 12);
-    this.drawRobot(ctx, path, config);
-  }
-
-  drawRobot(ctx, path, config) {
-    const setpoint = path.sourceSetpoints[this.props.rangePosition];
-    if (setpoint === undefined) return;
-    ctx.beginPath();
-    const w = (Number(config.width) + 0.2) / this.props.fieldConfig.widthPixelToMeter + 5;
-    const h = (Number(config.width) + 0.2) / this.props.fieldConfig.widthPixelToMeter;
-    const x = setpoint.x / this.props.fieldConfig.widthPixelToMeter - w / 2 +
-      this.props.fieldConfig.topLeftXPixel;
-    const y = setpoint.y / this.props.fieldConfig.hightPixelToMeter - h / 2 +
-      this.props.fieldConfig.topLeftYPixel;
-
-    ctx.save();
-    ctx.translate(x + w / 2, y + h / 2);
-    ctx.rotate(setpoint.heading);
-    ctx.fillStyle = "white";
-    ctx.fillRect(-w / 2, -h / 2, w, h);
-    ctx.restore();
+    drawOnCanvas(this.canvasRef.current, this.props);
   }
 
   whenClick(event) {
@@ -112,16 +36,6 @@ class FieldView extends React.Component {
     const y = ((event.clientY - rect.top) * scaleY - this.props.fieldConfig.topLeftYPixel)
       * this.props.fieldConfig.hightPixelToMeter;
     this.props.addWaypoint(new PathWaypoint(x, y));
-  }
-
-  componentDidUpdate() {
-    const canvas = this.canvasRef.current;
-    if (this.props.listenToMouseClicks)
-      canvas.addEventListener('mousedown', this.whenClick);
-    else
-      canvas.removeEventListener('mousedown', this.whenClick);
-
-    this.drawField();
   }
 
   render() {
@@ -142,7 +56,7 @@ class FieldView extends React.Component {
 const mapStateToProps = (state) => {
   return {
     listenToMouseClicks: state.listenToMouseClicks,
-    clearFieldView: state.clearFieldView,
+    robotDrawConfig: state.robotDrawConfig,
     rangePosition: state.rangePosition,
     fieldConfig: state.fieldConfig,
     pathConfig: state.pathConfig,
