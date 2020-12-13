476f1544ef6e7b68b5a1ec4fb7a1d7e2dea834be
Matan Steinmetz <matanabc@gmail.com>
2020-12-13 17:42:11 +0200

    Add: view-config

--- src/component/app.js
+++ src/component/app.js
@@ -2,7 +2,7 @@ import WaypointsList from './waypoints/waypoints-list';
 import { Container } from 'react-bootstrap';
 import PlayingBar from './playing-bar';
 import { connect } from 'react-redux';
-import FieldView from './field-view';
+import FieldView from './field-view/field-view';
 import Tools from './ tools';
 import React from 'react';
 
---
+++ src/component/field-view/view-config.js
@@ -0,0 +1,28 @@
+export class FieldConfig {
+	constructor(
+		widthInMeter = 16.5354,
+		heightInMeter = 8.001,
+		topLeftXPixel = 0,
+		topLeftYPixel = 0,
+		widthInPixel = 0,
+		heigthInPixel = 0
+	) {
+		this.widthInMeter = Number(widthInMeter);
+		this.heightInMeter = Number(heightInMeter);
+		this.topLeftXPixel = Number(topLeftXPixel);
+		this.topLeftYPixel = Number(topLeftYPixel);
+		this.widthInPixel = Number(widthInPixel);
+		this.heigthInPixel = Number(heigthInPixel);
+
+		this.widthPixelToMeter = widthInPixel === 0 ? 0 : widthInMeter / widthInPixel;
+		this.hightPixelToMeter = heigthInPixel === 0 ? 0 : heightInMeter / heigthInPixel;
+	}
+}
+
+export class RobotDrawConfig {
+	constructor(width = 0.8, length = 1.2, center = 0) {
+		this.center = center;
+		this.length = length;
+		this.width = width;
+	}
+}
