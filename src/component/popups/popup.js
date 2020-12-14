ff49da79eb311424f79b50cf0806110d3b753716
Matan Steinmetz <matanabc@gmail.com>
2020-12-14 07:56:47 +0200

    Add: popup

---
+++ src/component/popups/popup.js
@@ -0,0 +1,48 @@
+import { Modal, Button } from 'react-bootstrap';
+import React from 'react';
+
+class Popup extends React.Component {
+	constructor(props) {
+		super(props);
+		this.cancelButtonRef = React.createRef();
+	}
+
+	componentDidUpdate() {
+		if (!this.props.show) return;
+		if (this.props.refToUse !== undefined && this.props.refToUse.current !== null)
+			this.props.refToUse.current.focus();
+		else if (this.cancelButtonRef.current !== null) this.cancelButtonRef.current.focus();
+	}
+
+	render() {
+		var confirmButton = <Button onClick={this.props.confirm}>confirm</Button>;
+		var cancelButton = (
+			<Button variant="outline-primary" onClick={this.props.close} ref={this.cancelButtonRef}>
+				cancel
+			</Button>
+		);
+		if (this.props.confirm === undefined) {
+			confirmButton = <span />;
+			cancelButton = (
+				<Button ref={this.cancelButtonRef} onClick={this.props.close}>
+					ok
+				</Button>
+			);
+		}
+
+		return (
+			<Modal show={this.props.show} onHide={this.props.close}>
+				<Modal.Header>
+					<Modal.Title>{this.props.title}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>{this.props.body}</Modal.Body>
+				<Modal.Footer>
+					{cancelButton}
+					{confirmButton}
+				</Modal.Footer>
+			</Modal>
+		);
+	}
+}
+
+export default Popup;
