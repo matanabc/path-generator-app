465743a1205956413fe9a0539575f9d73fc3c81a
Matan Steinmetz <matanabc@gmail.com>
2020-12-17 09:49:46 +0200

    Remove: path-generator folder

--- src/path-generator/path-generator.js
+++
@@ -1,9 +0,0 @@
-import Generator from "./generator";
-import Config from "./path-config";
-import Waypoint from "./waypoint";
-import { tank } from "./modifier";
-
-export const PathGenerator = Generator;
-export const PathConfig = Config;
-export const PathWaypoint = Waypoint;
-export const TankModifier = tank;
