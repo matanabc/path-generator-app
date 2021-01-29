86345f8b173f7eae95a807b91791ed9409227b3e
Matan Steinmetz <matanabc@gmail.com>
2021-01-29 11:08:51 +0200

    Fix: listenToMouseClicks will be disable after settings have change

--- src/redux/project/reducer.js
+++ src/redux/project/reducer.js
@@ -10,7 +10,7 @@ import {
 
 function setSettings(state, payload) {
 	saveJsonProject(payload.settings);
-	return { ...state, ...payload.settings, selectedPath: undefined };
+	return { ...state, ...payload.settings, selectedPath: undefined, listenToMouseClicks: false };
 }
 
 function setProjectPath(state, payload) {
