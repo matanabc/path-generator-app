703cf23e37f3681e0bdeb8e15740216142644211
Matan Steinmetz <matanabc@gmail.com>
2021-10-08 13:36:35 +0300

    Add: thunk middleware for Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,16 @@
-import { initialState } from './initial-state';
+import { createStore, applyMiddleware } from 'redux';
 import reduceReducers from 'reduce-reducers';
-import group from './group/reducer';
+import thunk from 'redux-thunk';
+
+import { initialState } from './initial-state';
 import project from './project/reducer';
-import { createStore } from 'redux';
+import group from './group/reducer';
 import view from './view/reducer';
 import path from './path/reducer';
 import app from './app/reducer';
 
 const reducers = reduceReducers(initialState, view, path, project, app, group);
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 if (process.env.NODE_ENV === 'development')
 	store.subscribe(() => {
