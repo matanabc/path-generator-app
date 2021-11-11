5262ecaa818b6108b5b9afa700adb022f1231f8b
Matan Steinmetz <matanabc@gmail.com>
2021-11-11 22:13:19 +0200

    Add: PlayingBar

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,10 +1,13 @@
 import { SimulationView } from './simulation';
+import PlayingBar from './playing-bar';
+
 import './app.css';
 
 export default function App() {
 	return (
 		<div className='App'>
 			<SimulationView filedImageUrl={''} />
+			<PlayingBar />
 		</div>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
