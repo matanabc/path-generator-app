c8c807784516cf909a749a06f35b0647eea1b68c
Matan Steinmetz <matanabc@gmail.com>
2021-11-26 12:16:53 +0200

    Add: preventDefault

--- src/component/common/button.tsx
+++ src/component/common/button.tsx
@@ -1,15 +1,20 @@
+import mousetrap, { ExtendedKeyboardEvent } from 'mousetrap';
 import { Button as BootstrapButton } from 'react-bootstrap';
 import { useEffect } from 'react';
-import mousetrap from 'mousetrap';
 import 'mousetrap-global-bind';
 
 import { TButtonProps } from './types';
 
 export default function Button(props: TButtonProps) {
+	const onClick = (e: ExtendedKeyboardEvent, combo: string) => {
+		e.preventDefault();
+		props.onClick(e, combo);
+	};
+
 	useEffect(() => {
 		if (!props.shortcut) return;
 		mousetrap.unbind(props.shortcut);
-		mousetrap.bind(props.shortcut, props.onClick);
+		mousetrap.bind(props.shortcut, onClick);
 	});
 
 	return <BootstrapButton {...props} />;
--- src/shortcut.ts
+++ src/shortcut.ts
@@ -1,1 +1,1 @@
-export const SETTINGS_SHORTCUT = ['command+p', 'alt+p'];
+export const SETTINGS_SHORTCUT = ['command+p', 'ctrl+p'];
