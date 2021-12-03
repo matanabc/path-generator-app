7b0a73fbbba3b91936c9c6611c402985b8e5b6c8
Matan Steinmetz <matanabc@gmail.com>
2021-12-03 15:55:08 +0200

    Add: change waypoint angle

--- src/component/simulation/waypoint.tsx
+++ src/component/simulation/waypoint.tsx
@@ -1,4 +1,5 @@
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
+import { useEffect, useRef } from 'react';
 
 import { BORDER_SIZE, WAYPOINT_SIZE } from '../../consts';
 import { materToPixel, pixelToMater } from '../../util';
@@ -9,6 +10,7 @@ const minOffset = WAYPOINT_SIZE / 2 - BORDER_SIZE / 2;
 const maxOffset = WAYPOINT_SIZE / 2 + BORDER_SIZE / 2;
 
 export default function Waypoint({ index, waypoint, setWaypoint }: TWaypointProps) {
+	const waypointRef = useRef<HTMLDivElement>(null);
 	const { x, y } = materToPixel(waypoint.x, waypoint.y);
 	const { topLeftX, topLeftY, widthInPixel, heightInPixel } = useFieldStore();
 	const position = { x: x + topLeftX - minOffset, y: y + topLeftY - minOffset };
@@ -17,12 +19,21 @@ export default function Waypoint({ index, waypoint, setWaypoint }: TWaypointProp
 		...{ right: topLeftX + widthInPixel - maxOffset, bottom: topLeftY + heightInPixel - maxOffset },
 	};
 
+	const onWheel = (e: WheelEvent) => setWaypoint(index, { angle: waypoint.angle + e.deltaY * 0.1 });
 	const onDrag = (e: DraggableEvent, { x, y }: DraggableData) =>
 		setWaypoint(index, pixelToMater(x - topLeftX + minOffset, y - topLeftY + minOffset));
 
+	useEffect(() => {
+		const waypoint = waypointRef.current;
+		if (waypoint === null) return;
+		waypoint.onwheel = onWheel;
+	});
+
 	return (
 		<Draggable position={position} bounds={bounds} onDrag={onDrag}>
-			<div id='Waypoint'>{index + 1}</div>
+			<div id='Waypoint' ref={waypointRef}>
+				{index + 1}
+			</div>
 		</Draggable>
 	);
 }
